f6c74aa06f1cee450dbaca0120c695769f5dde3f
Joaquin <18263598+Joaco853@users.noreply.github.com>
2023-02-18 17:04:27 -0300

    Inicio Rutinario

---
+++ Projects/Rutinario/jsrutinas.js
@@ -0,0 +1,2 @@
+// El agregar rutinas hacerlo o por botones seleccionando el dia : elegir ejercio y elegir el dia al cual agregar o por arrastrar el ejercicio
+//que el buscador se despliegue desde la derecha
